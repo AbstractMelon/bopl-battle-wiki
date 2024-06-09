bfdfb7fd6338cc7cd67097f7134c5efef58e4884
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-06-08 21:46:52 -0500

    aaaaaaaaa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,11 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+            <head>
+            <meta name="viewport" content="height=device-height,
+          width=device-width, initial-scale=1.0,
+          minimum-scale=1.0" />
+            </head>
             <body className={inter.className}>{children}</body>
         </html>
     );
